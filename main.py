ba9ad75e43663a0d47d8c2b4122ca745a4041c91
Jack Freeman <jfreeman@morgridge.org>
2022-10-27 15:23:38 -0500

    test

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    print("hello calculator")
+    a = 2
+    b = 4
+    c = multiply(a, b)
+    print(str(a) + "*" + str(b) + "=" + str(c))
+
+
+def multiply(x, y):
+    return x / y
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_calc.py
@@ -0,0 +1,6 @@
+import main
+
+
+def test_multiply():
+    test_multi_val = main.multiply(3, 5)
+    assert test_multi_val == 15
